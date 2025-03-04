ef43f5e38d7dc9578d1c8f750a331610fb4c6ca5
josiasmartins <josiasmartinscaitano@gmail.com>
2025-03-04 13:37:19 -0300

    feature: removed space

--- ProducerSpringkafka/src/main/java/br/com/josiasmartins/springkafka/SpringkafkaApplication.java
+++ ProducerSpringkafka/src/main/java/br/com/josiasmartins/springkafka/SpringkafkaApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringkafkaApplication {
 
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringkafkaApplication.class, args);
 	}
